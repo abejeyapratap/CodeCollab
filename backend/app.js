a145345a24fed07e3de6e2238968d02ed3dfa568
Abe Jeyapratap <abejeyapratap@gmail.com>
2022-03-01 12:57:07 -0500

    enabled CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,21 @@ const express = require("express");
 
 const app = express();
 
+// CORS Middleware
+const ANGULAR_ORIGIN = "http://localhost:4200";
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", ANGULAR_ORIGIN); // filter domain
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    ); // filter headers
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+    next();
+});
+
 app.get("/", (req, res) => res.send("Hello"));
 
 module.exports = app;
