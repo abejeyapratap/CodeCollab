7b0e9935d2f6e531b0542d18bb69f77f5af02f7b
Abe Jeyapratap <abejeyapratap@gmail.com>
2022-03-02 11:21:36 -0500

    mongodb user schema

---
+++ backend/models/auth.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    googleId: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
+    displayName: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", userSchema);
