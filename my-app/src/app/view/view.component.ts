5bf8915dd16d400216297d9a353802ced5c58484
RyfiGames <69179298+RyfiGames@users.noreply.github.com>
2022-03-05 14:45:51 -0500

    Chat renders in the UI now

--- my-app/src/app/view/view.component.ts
+++ my-app/src/app/view/view.component.ts
@@ -10,12 +10,29 @@ export class ViewComponent implements OnInit {
 
   constructor(private sockets:SocketService) { }
 
+  messages: MessageData[] = [];
+  myDisplayName: string = 'p';
+
   ngOnInit(): void {
     this.sockets.onNewChatMessage().subscribe((data) => { this.addChatMessage(data[0], data[1], data[2], data[3]) })
   }
 
-  addChatMessage(username:string, icon:string, msg:string, date:number): void {
+  addChatMessage(username: string, icon: string, msg: string, date: number): void {
+    this.messages.push(new MessageData(username, icon, msg, date));
     console.log(msg);
   }
 
+}
+
+class MessageData {
+  username: string;
+  icon: string;
+  text: string;
+  date: number;
+  constructor(username:string, icon:string, msg:string, date:number) {
+    this.username = username;
+    this.icon = icon;
+    this.text = msg;
+    this.date = date;
+  }
 }
