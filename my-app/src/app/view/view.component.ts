5a407c66cb16f05706b70560cef21720104ad640
lincolnwu <lincolnwu18@gmail.com>
2022-03-08 12:43:21 -0500

    read code into box

--- my-app/src/app/view/view.component.ts
+++ my-app/src/app/view/view.component.ts
@@ -87,7 +87,11 @@ export class ViewComponent implements OnInit {
   loadTextarea () {
     let viewBox = <HTMLInputElement>document.getElementById("viewBox");
     viewBox.value = sessionStorage.getItem("textContent")!;
-    console.log(viewBox.value)
+    //console.log(viewBox.value)
+    let arrayValues = viewBox.value.split("\n")
+    //console.log("Print these values: " ,arrayValues);
+    this.documentLines = arrayValues;
+    //return arrayValues;
   }
 }
 
