5f9892824a742d56841e83280f9ccc0fe1bcdc0c
RyfiGames <69179298+RyfiGames@users.noreply.github.com>
2022-03-08 01:21:05 -0500

    We can now view documents

--- my-app/src/app/view/view.component.ts
+++ my-app/src/app/view/view.component.ts
@@ -31,7 +31,7 @@ export class ViewComponent implements OnInit {
   newMessage = '';
   myDisplayName: string = '';
 
-  documentLines:string[] = ["test1", "test2", "test my shoe"]
+  documentLines:string[] = ["function foo() {", "\treturn 0;", "}"]
 
   ngOnInit(): void {
     this.sockets.onNewChatMessage().subscribe((data) => { this.addChatMessage(data[0], data[1], data[2], data[3]) })
