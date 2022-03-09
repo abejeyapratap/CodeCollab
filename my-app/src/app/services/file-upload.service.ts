e3f073be9e459cccb77a36d3364560b925f309b6
lincolnwu <lincolnwu18@gmail.com>
2022-03-08 22:22:21 -0500

    added some comments

--- my-app/src/app/services/file-upload.service.ts
+++ my-app/src/app/services/file-upload.service.ts
@@ -9,25 +9,32 @@ import { HttpErrorResponse, HttpClient } from '@angular/common/http';
 export class FileUploadService {
   constructor(private http: HttpClient) {}
 
-  addUser(name: string, profileImage: string): Observable<any> {
+  // Use FormData object to store the uploaded filename (that we set manually) and its file
+  addDocument(filename: string, codeContent: string): Observable<any> {
+    // Create new FormData
     var formData: any = new FormData();
-    formData.append('name', name);
-    formData.append('codeContent', profileImage);
+    // Append the filename and contents to FormData
+    formData.append('name', filename);
+    formData.append('codeContent', codeContent);
 
+    // Post request to API to send to MongoDB database
     return this.http
       .post('http://localhost:3000/api/documents/create-document', formData, {
         reportProgress: true,
         observe: 'events',
       })
+      // Error handling
       .pipe(catchError(this.errorMgmt));
   }
 
-  getUser(): Observable<any> {
+  // Get request to API to retrieve most recent entry from database (using currID from document.js) 
+  getDocument(): Observable<any> {
     return this.http
       .get('http://localhost:3000/api/documents/get-document')
       .pipe(catchError(this.errorMgmt));
   }
 
+  // Error handling
   errorMgmt(error: HttpErrorResponse) {
     let errorMessage = '';
     if (error.error instanceof ErrorEvent) {
