1e6eb51fc1367b720470e8605a22004bd5e35ce8
lincolnwu <lincolnwu18@gmail.com>
2022-03-10 14:00:06 -0500

    socket package

--- my-app/src/app/services/socket.service.ts
+++ my-app/src/app/services/socket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
-import { map } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
